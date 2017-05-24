57d49a83a7594f754ab5987c9e7af2c5c088ed12
William Van Hevelingen <william.vanhevelingen@acquia.com>
2017-05-24 16:24:16 -0400

    Add more schema

--- FoodProcessor.rb
+++ FoodProcessor.rb
@@ -33,12 +33,51 @@ end
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/foodprocessor.db")
 
+
+class Unit
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, Text, :required => true, :unique => true
+  belongs_to :item
+end
+
 class Item
   include DataMapper::Resource
+
+  has 1, :unit
   property :id, Serial
   property :name, Text, :required => true, :unique => true
   property :required, Boolean, :required => true, :default => false
+  property :quantity, Integer, :required => true, :default => 1
+  belongs_to :event
 end
 
+class Event
+  include DataMapper::Resource
+
+  has n, :items
+  has 1, :link
+  property :id, Serial
+  property :name, Text, :required => true, :unique => true
+  property :date, DateTime, :required => true
+  property :created_at, DateTime, :required => true, :default => lambda{ |p,s| DateTime.now}
+  property :location, Text
+end
+
+class Link
+  include DataMapper::Resource
+
+  property :id, Serial
+  belongs_to :event
+end
+
+class User
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, Text, :required => true
+  property :email, Text, :required => true
+end
 
 DataMapper.finalize.auto_upgrade!
