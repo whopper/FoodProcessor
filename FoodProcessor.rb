37fe3bd8b7cb400e366b6bd178ab3eb88fd36714
Brianna Buckner <brianna.buckner@acquia.com>
2017-05-24 15:28:24 -0400

    make the db save items

--- FoodProcessor.rb
+++ FoodProcessor.rb
@@ -1,6 +1,27 @@
 require 'sinatra'
+require 'data_mapper'
 
 get '/' do
-  @test = 'Hello World'
+  @items = Item.all :order => :id.desc
   erb :home
 end
+
+post '/' do
+  item = Item.new
+  item.name = params[:content]
+  item.required = params[:required].nil? ? false : params[:required]
+  item.save
+  redirect '/'
+end
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/foodprocessor.db")
+
+class Item
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, Text, :required => true, :unique => true
+  property :required, Boolean, :required => true, :default => false
+end
+
+
+DataMapper.finalize.auto_upgrade!
--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 
 gem 'sinatra'
+gem 'shotgun'
 gem 'sqlite3'
 gem 'datamapper'
 gem 'dm-sqlite-adapter'
